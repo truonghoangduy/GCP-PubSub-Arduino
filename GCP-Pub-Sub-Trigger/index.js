1fe90d8c6169282223d2fbea6cdb7a4db85e1736
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-06-11 05:17:48 +0700

    Create Devices with default info or update relevant detail

--- GCP-Pub-Sub-Trigger/index.js
+++ GCP-Pub-Sub-Trigger/index.js
@@ -1,19 +1,55 @@
 const { PubSub } = require('@google-cloud/pubsub');
 
-const { Firestore } = require('@google-cloud/firestore')
+const { Firestore, DocumentReference } = require('@google-cloud/firestore')
 
 const pubsub = new PubSub()
 const firestore = new Firestore()
+const blankTemplet = {
+    "maintence": 0,
+    "numberoffushing": 0,
+    "numberofrefill": 0,
+    "watermaintence": 0
+}
+const switchCaseMap = {
+    '0': "maintence",
+    '1': "numberoffushing",
+    "2": "numberofrefill",
+    "3": "watermaintence"
+}
 
-exports.subscribe = (pubsubMessage) => {
+exports.subscribe = async (pubsubMessage) => {
+
     // Print out the data from Pub/Sub, to prove that it worked
-    // firebase.database.ref("duy-beo-test").
-    let message = Buffer.from(pubsubMessage.data, 'base64').toString()
+    // {
+    //     "data": string,
+    //     "attributes": {
+    //       string: string,
+    //       ...
+    //     },
+    //     "messageId": string,
+    //     "publishTime": string
+    //   }
 
-    firestore.collection("duy-beo-test").doc().set({
-        "message"
-            : message
-    })
-    // console.log(Buffer.from(pubsubMessage.data, 'base64').toString());
+    // Reuqiew data 
+    // docid: deviceid
+    // maintence: 1
+    // (number) 
+    // numberoffushing: 11
+    // (number) 
+    // numberofrefill: 2
+    // (number) 
+    // watermaintence: 84
+
+
+    let message = Buffer.from(pubsubMessage.data, 'base64').toString(); // DECODE message
+    let documentRef = await firestore.collection("Device").doc(pubsubMessage.attributes.deviceId).get();
+    if (documentRef.exists) {
+        let data = {};
+        data[switchCaseMap[message]] = documentRef.get(switchCaseMap[message]) + 1
+        await firestore.collection("Device").doc(pubsubMessage.attributes.deviceId).update(data)
+    } else {
+        await firestore.collection("Device").doc(pubsubMessage.attributes.deviceId).set(Object.assign(blankTemplet,pubsubMessage.attributes));
+    }
+    console.log(pubsubMessage)
 
 };
